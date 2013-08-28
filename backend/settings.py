608d3cb53e6eb56edcd94286e7f78f7affcbe46b
rubillionaire <ruben@rubillionaire.com>
2013-08-28 08:33:10 -0400

    social auth settings

--- backend/settings.py
+++ backend/settings.py
@@ -225,4 +225,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 SOCIAL_AUTH_ENABLED_BACKENDS = ('twitter', 'facebook')
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_auth.backends.pipeline.social.social_auth_user',
+    #'social_auth.backends.pipeline.associate.associate_by_email',
+    'social_auth.backends.pipeline.user.get_username',
+    'social_auth.backends.pipeline.user.create_user',
+    'social_auth.backends.pipeline.social.associate_user',
+    'social_auth.backends.pipeline.social.load_extra_data',
+    'social_auth.backends.pipeline.user.update_user_details'
+)
 ### end Social auth config
