e16410df7ed9102e4d11360c7a06676c6f374224
rubillionaire <mail@rubenrodriguez.me>
2013-11-08 15:16:02 -0500

    update logging on clocks
    for heroku

--- clock.py
+++ clock.py
@@ -1,25 +1,28 @@
+import logging
 import subprocess
 import atexit
 
 from apscheduler.scheduler import Scheduler
 
+logger = logging.getLogger(__name__)
 sched = Scheduler(daemon=True)
 atexit.register(lambda: sched.shutdown(wait=False))
 
 
 @sched.interval_schedule(minutes=1)
 def update_from_apis():
-    print "This job will update twitter and tumblr"
     tweets = subprocess.check_call(['foreman',
                                     'run',
                                     'python',
                                     'manage.py',
                                     'worker_twitter'])
+    logger.info(tweets)
     tumblr = subprocess.check_call(['foreman',
                                     'run',
                                     'python',
                                     'manage.py',
                                     'worker_tumblr'])
+    logger.info(tumblr)
 
 sched.start()
 
