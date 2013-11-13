81f7bd465382d351233c1d1493177e40469d7495
rubillionaire <mail@rubenrodriguez.me>
2013-11-13 13:21:19 -0500

    another clock attempt

--- clock.py
+++ clock.py
@@ -12,22 +12,16 @@ atexit.register(lambda: sched.shutdown(wait=False))
 
 @sched.interval_schedule(minutes=1)
 def update_from_apis():
-    tweets = subprocess.check_call(['foreman',
-                                    'run',
-                                    'python',
+    tweets = subprocess.check_call(['python',
                                     'manage.py',
                                     'worker_news_tweets'])
     logger.info(tweets)
-    tumblr = subprocess.check_call(['foreman',
-                                    'run',
-                                    'python',
+    tumblr = subprocess.check_call(['python',
                                     'manage.py',
                                     'worker_news_tumbls'])
 
     logger.info(tumblr)
-    hash_tweets = subprocess.check_call(['foreman',
-                                         'run',
-                                         'python',
+    hash_tweets = subprocess.check_call(['python',
                                          'manage.py',
                                          'worker_hash_tweets'])
     logger.info(hash_tweets)
